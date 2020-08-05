b5209b69b88713287ae4d673fecae4b8d7812b86
joe zhang <joe.zhang@ringcentral.com>
2020-08-05 21:54:14 +0800

    add controller

---
+++ Catalog-API/src/main/java/com/eshop/api/catalog/controller/CatalogController.java
@@ -0,0 +1,9 @@
+package com.eshop.api.catalog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/catalog")
+public class CatalogController {
+}
---
+++ Catalog-API/src/main/java/com/eshop/api/catalog/controller/PicController.java
@@ -0,0 +1,10 @@
+package com.eshop.api.catalog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PicController {
+
+
+}
