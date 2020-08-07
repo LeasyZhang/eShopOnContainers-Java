bb53b06876239d953b2888de8825c3a131e80bf4
joe zhang <joe.zhang@ringcentral.com>
2020-08-07 23:47:40 +0800

    add request mapping

--- Catalog-API/src/main/java/com/eshop/api/catalog/controller/CatalogController.java
+++ Catalog-API/src/main/java/com/eshop/api/catalog/controller/CatalogController.java
@@ -1,9 +1,32 @@
 package com.eshop.api.catalog.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eshop.api.catalog.model.CatalogItem;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/catalog")
 public class CatalogController {
+
+    @RequestMapping("/items[?pageSize=3&pageIndex=10]\n")
+    public List<CatalogItem> getItems(@RequestBody(required = false) List<Integer> idList, @RequestParam Integer pageSize, @RequestParam Integer pageIndex) {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping("/item/{id}")
+    public CatalogItem getItemById(@PathVariable("id") Integer id) {
+        return new CatalogItem();
+    }
+
+    @RequestMapping("/items/withname/{name}")
+    public List<CatalogItem> itemsWithName(@PathVariable("name") String name) {
+        return new ArrayList<>();
+    }
+
+    @RequestMapping("")
+    public List<CatalogItem> itemsByCatalogAndBrandId() {
+
+    }
 }
