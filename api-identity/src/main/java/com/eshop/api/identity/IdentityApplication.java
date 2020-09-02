812dc8c9b4c7501cec4597d477afe8cac80f7796
joe zhang <joe.zhang@ringcentral.com>
2020-09-02 22:10:53 +0800

    identity application

---
+++ api-identity/src/main/java/com/eshop/api/identity/IdentityApplication.java
@@ -0,0 +1,12 @@
+package com.eshop.api.identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityApplication.class, args);
+    }
+}
